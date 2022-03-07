6d0a5424a02b10d8b8b38c19005f49e2ee4d0f2e
Kazi Jawad <kazi.a.jawad@gmail.com>
2022-03-07 16:24:52 -0500

    refactor Texture to Texture2D

--- src/core/index.js
+++ src/core/index.js
@@ -1,3 +1,3 @@
 export { Program } from './program.js';
 export { Renderer } from './renderer.js';
-export { Texture } from './texture.js';
+export { Texture2D } from './texture.js';
--- src/core/texture.js
+++ src/core/texture.js
@@ -1,31 +1,36 @@
-export class Texture {
-    constructor({
-        gl,
-        src,
-        wrapS,
-        wrapT,
-        minFilter,
-        magFilter,
-    }) {
-        // Initialization
+export class Texture2D {
+    constructor(gl, value, wrapS = gl.CLAMP_TO_EDGE, wrapT = gl.CLAMP_TO_EDGE, minFilter = gl.NEAREST, magFilter = gl.NEAREST) {
         this.gl = gl;
-        this.src = src;
-        this.wrapS = wrapS || this.gl.CLAMP_TO_EDGE;
-        this.wrapT = wrapT || this.gl.CLAMP_TO_EDGE;
-        this.minFilter = minFilter || this.gl.NEAREST;
-        this.magFilter = magFilter || this.gl.NEAREST;
+        this.value = value;
 
-        // Creation 
-        this.glTexture = this.gl.createTexture();
-        this.gl.bindTexture(this.gl.TEXTURE_2D, this.glTexture);
+        this.source = this.gl.createTexture();
 
-        // Parameters
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.wrapS);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.wrapT);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.minFilter);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.magFilter);
+        this.wrapS = wrapS;
+        this.wrapT = wrapT;
+        this.minFilter = minFilter;
+        this.magFilter = magFilter;
 
-        // Image
-        this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, this.src);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.source);
+        this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, this.value);
+    }
+
+    set wrapS(value) {
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.source);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, value);
+    }
+
+    set wrapT(value) {
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.source);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, value);
+    }
+
+    set minFilter(value) {
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.source);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, value);
+    }
+
+    set magFilter(value) {
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.source);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, value);
     }
 }
